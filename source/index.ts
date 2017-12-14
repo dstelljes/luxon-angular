f5d21fa429a9a19e23da36d15448b622a2fef13d
Dan Stelljes <dan@stellj.es>
2017-12-13 22:28:55 -0600

    Organize pipes directory.

--- source/index.ts
+++ source/index.ts
@@ -1,21 +1,21 @@
 import { NgModule } from "@angular/core";
 
-import { DateTimeFromFormatPipe } from "./pipes/date-time-from-format";
-import { DateTimeFromHttpPipe } from "./pipes/date-time-from-http";
-import { DateTimeFromIsoPipe } from "./pipes/date-time-from-iso";
-import { DateTimeFromJsDatePipe } from "./pipes/date-time-from-js-date";
-import { DateTimeFromMillisecondsPipe } from "./pipes/date-time-from-milliseconds";
-import { DateTimeFromRfc2822Pipe } from "./pipes/date-time-from-rfc-2822";
-import { DateTimeFromSqlPipe } from "./pipes/date-time-from-sql";
-import { DateTimeToFormatPipe } from "./pipes/date-time-to-format";
-import { DateTimeToIsoDatePipe } from "./pipes/date-time-to-iso-date";
-import { DateTimeToIsoTimePipe } from "./pipes/date-time-to-iso-time";
-import { DateTimeToIsoWeekDatePipe } from "./pipes/date-time-to-iso-week-date";
-import { DateTimeToIsoPipe } from "./pipes/date-time-to-iso";
-import { DateTimeToJsDatePipe } from "./pipes/date-time-to-js-date";
-import { DateTimeToLocalPipe } from "./pipes/date-time-to-local";
-import { DateTimeToSqlPipe } from "./pipes/date-time-to-sql";
-import { DateTimeToUtcPipe } from "./pipes/date-time-to-utc";
+import { DateTimeFromFormatPipe } from "./pipes/parsing/date-time-from-format";
+import { DateTimeFromHttpPipe } from "./pipes/parsing/date-time-from-http";
+import { DateTimeFromIsoPipe } from "./pipes/parsing/date-time-from-iso";
+import { DateTimeFromJsDatePipe } from "./pipes/parsing/date-time-from-js-date";
+import { DateTimeFromMillisecondsPipe } from "./pipes/parsing/date-time-from-milliseconds";
+import { DateTimeFromRfc2822Pipe } from "./pipes/parsing/date-time-from-rfc-2822";
+import { DateTimeFromSqlPipe } from "./pipes/parsing/date-time-from-sql";
+import { DateTimeToFormatPipe } from "./pipes/formatting/date-time-to-format";
+import { DateTimeToIsoDatePipe } from "./pipes/formatting/date-time-to-iso-date";
+import { DateTimeToIsoTimePipe } from "./pipes/formatting/date-time-to-iso-time";
+import { DateTimeToIsoWeekDatePipe } from "./pipes/formatting/date-time-to-iso-week-date";
+import { DateTimeToIsoPipe } from "./pipes/formatting/date-time-to-iso";
+import { DateTimeToJsDatePipe } from "./pipes/formatting/date-time-to-js-date";
+import { DateTimeToLocalPipe } from "./pipes/zones/date-time-to-local";
+import { DateTimeToSqlPipe } from "./pipes/formatting/date-time-to-sql";
+import { DateTimeToUtcPipe } from "./pipes/zones/date-time-to-utc";
 
 const PIPES = [
   DateTimeFromFormatPipe,
